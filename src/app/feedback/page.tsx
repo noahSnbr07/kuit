8b4ed06d39ed07035d826d7a074be755216f14a5
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-06-04 23:16:34 +0200

    added little heart :3

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -2,16 +2,12 @@
 
 import Form from "./components/form";
 
-interface _props {
-
-}
-
-export default async function page({ }: _props) {
+export default async function page() {
 
 
     return (
         <div className="flex flex-col gap-4 p-4">
-            <b> Help to improve my app </b>
+            <b> Help to improve my app {"<3"} </b>
             <Form />
         </div>
     );
